3b9750c293e185a734ecdcf008e7ed2a4571df2d
ericvinicius <eric.camargo@gmail.com>
2022-09-08 18:15:05 -0300

    adding basic movements

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float speed;
+    public Rigidbody2D rig;
+
+    public float direction_x;
+    public float direction_y;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        direction_x = Input.GetAxis("Horizontal");
+        direction_y = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate() {
+        rig.velocity = new Vector2(direction_x * speed, direction_y * speed);
     }
 }
